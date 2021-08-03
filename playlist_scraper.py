78ae80317980768cf63bbad31c1823904146dbb0
Thijmen Havinga <thijmenhavinga@gmail.com>
2021-08-03 19:38:31 +0200

    Added conditional for empty ghost tracks

--- playlist_scraper.py
+++ playlist_scraper.py
@@ -1,7 +1,10 @@
-import os
-from mysql.connector import connect, Error
+import pprint
+from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+from mysql.connector import connect, Error
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # ---------- Playlist URI's ----------
 rr = 'spotify:playlist:37i9dQZEVXbeXrGsbZu97Y'
@@ -10,6 +13,8 @@ dm1 = 'spotify:playlist:37i9dQZF1E36Bn1AIzrRAZ'
 
 
 # ---------- Scrape info from playlists ----------
+
+
 def scraper(playlist):
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyClientCredentials()
@@ -17,27 +22,29 @@ def scraper(playlist):
 
     # response returns JSON-text. First element is the list of items.
     response = spotify.playlist_items(
-        playlist_id=playlist, fields='items.track')['items']
+        playlist_id=playlist, fields='items[0].track')['items']
 
     result = []
-
     for item in response:
-        album_artist = item['track']['album']['artists'][0]['name']
-
-        # If multiple artists on track: artists seperated by comma
-        if len(item['track']['artists']) > 1:
-            artists = []
-            for a in item['track']['artists']:
-                artists.append(a['name'])
-            artists = ", ".join(artists)
+        if item['track'] == None:
+            continue
         else:
-            artists = album_artist
+            album_artist = item['track']['album']['artists'][0]['name']
 
-        title = item['track']['name']
-        sp_uri = item['track']['uri']
-        playlist_id = 1 if (playlist == rr) else (
-            2 if playlist == dw else 3)
-        result.append([album_artist, artists, title, sp_uri, playlist_id])
+            # If multiple artists on track: artists seperated by comma
+            if len(item['track']['artists']) > 1:
+                artists = []
+                for a in item['track']['artists']:
+                    artists.append(a['name'])
+                artists = ", ".join(artists)
+            else:
+                artists = album_artist
+
+            title = item['track']['name']
+            sp_uri = item['track']['uri']
+            playlist_id = 1 if (playlist == rr) else (
+                2 if playlist == dw else 3)
+            result.append([album_artist, artists, title, sp_uri, playlist_id])
 
     return result
 
@@ -68,6 +75,6 @@ def save_to_db(data):
         print(e)
 
 
-if __name__ == "__main__":
-    save_to_db(scraper(rr))
-    save_to_db(scraper(dw))
+# if __name__ == "__main__":
+#     save_to_db(scraper(rr))
+#     save_to_db(scraper(dw))
